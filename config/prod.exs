640db86b485bdcc33d9fc361a292c553c3f20bee
MaxDac <massimiliano.dacunzo@hotmail.com>
2021-09-22 00:25:30 +0100

    Fixed configuration same-origin in Elixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :vtm_web, VtmWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "vtmbaires.eu", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
