2c9cb8befca682bbe4d5b948f0ffb255679b3077
MaxDac <massimiliano.dacunzo@hotmail.com>
2021-10-28 21:18:00 +0100

    Vampire the Masquerade: Redemption

--- apps/vtm_web/assets/src/components/Main.js
+++ apps/vtm_web/assets/src/components/Main.js
@@ -8,17 +8,25 @@ import Typography from '@mui/material/Typography';
 export default function Main(): any {
     const [firstPhrase, setFirstPhrase] = useState("");
     const [secondPhrase, setSecondPhrase] = useState("");
+    const [thirdPhrase, setThirdPhrase] = useState("");
 
     const firstLinePhrase = "Buenos Aires by Night";
     const secondLinePhrase = "Benvenuto";
+    const thirdLinePhrase = "It is a world of Darkness. The sin of Caine, has spawn a cursed horror that stalks the night" +
+        " in search of living blood. The kindreds have long been a secret influence through all ages of human history," +
+        " plotting against each other in a never-ending Jihad. Their immortal progeny are among us to this day, hidden" +
+        " from the eyes of humanity, by an elaborate... Masquerade.";
 
     useEffect(() => {
         const typeWriteLine = (setter: (string => string) => void, phrase: string, caret: number, onCompleted?: () => void) => {
             if (caret <= phrase.length) {
+                const newPhrase = phrase.substring(0, caret);
+                const time = newPhrase.substring(newPhrase.length - 1, newPhrase.length) === "." ? 300 : 150;
+
                 setTimeout(() => {
-                    setter(_ => phrase.substring(0, caret));
+                    setter(_ => newPhrase);
                     typeWriteLine(setter, phrase, caret + 1, onCompleted);
-                }, 150 * Math.random());
+                }, time * Math.random());
             }
             else {
                 if (onCompleted != null) {
@@ -28,7 +36,9 @@ export default function Main(): any {
         };
 
         typeWriteLine(setFirstPhrase, firstLinePhrase, 0, () => {
-            typeWriteLine(setSecondPhrase, secondLinePhrase, 0);
+            typeWriteLine(setSecondPhrase, secondLinePhrase, 0, () => {
+                typeWriteLine(setThirdPhrase, thirdLinePhrase, 0);
+            });
         });
     }, []);
 
@@ -61,6 +71,17 @@ export default function Main(): any {
                     // animationDelay: "3000ms"
                 }}>
                     {secondPhrase}
+                </Typography>
+                <Typography sx={{
+                    fontFamily: 'GabrieleLightRibbon',
+                    color: "#C9C9C9",
+                    fontSize: "16px",
+                    paddingTop: "20px"
+                    // opacity: 0,
+                    // animation: "opacityAppear 2s ease-in-out forwards",
+                    // animationDelay: "3000ms"
+                }}>
+                    {thirdPhrase}
                 </Typography>
             </CenteredBox>
         </Box>
