c7892cc12541d0ed088422e016ceef7c8fa8f395
MaxDac <massimiliano.dacunzo@hotmail.com>
2021-11-17 18:04:00 +0000

    Changed app version

--- apps/vtm_web/assets/src/_base/components/AppVersion.js
+++ apps/vtm_web/assets/src/_base/components/AppVersion.js
@@ -8,7 +8,7 @@ const AppVersion = (): any => {
         <Typography sx={{
             fontSize: "13px"
         }}>
-            App Version: 0.2.2.11.21111501
+            App Version: 0.2.2.12.21111701
         </Typography>
     );
 }
