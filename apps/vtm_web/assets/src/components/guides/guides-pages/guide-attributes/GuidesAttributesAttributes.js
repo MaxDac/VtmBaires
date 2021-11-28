23ead9fc688750bcc2ebe77d955697920f7e9f1b
MaxDac <massimiliano.dacunzo@hotmail.com>
2021-11-28 00:34:09 +0000

    Better specified the guide

--- apps/vtm_web/assets/src/components/guides/guides-pages/guide-attributes/GuidesAttributesAttributes.js
+++ apps/vtm_web/assets/src/components/guides/guides-pages/guide-attributes/GuidesAttributesAttributes.js
@@ -4,6 +4,8 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import {guideStyle, titleStyle} from "../../GuidesStyles";
+import { Link } from "react-router-dom";
+import { GuideRoutes } from "../../GuidesMain";
 
 type Props = {
     showAttributes: (string, string) => any[];
@@ -83,16 +85,19 @@ const GuidesAttributesAttributes = ({showAttributes}: Props): any => {
                         Le <b>Specialit&agrave;</b> sono delle specializzazioni in una determinata Abilit&agrave; del personaggio,
                         delle particolari classi di sapienza o di addestramento in cui il personaggio eccelle. Ogni specialit&agrave;
                         costituir&agrave; <b>un particolare sottoinsieme di una Abilit&agrave;</b>: ad esempio, una possibile 
-                        specialit&agrave; dell'Abilit&agrave; <b>Accademiche</b> potrebbe essere <b>Storia</b>, una di <b>Finanza</b>
-                        invece potrebbe essere <b>Criptomonete</b>, o <b>Mercato Azionario</b>. Non potr&agrave; invece essere 
-                        considerata una Specialit&agrave; consentita una forma di arte marziale per <b>Rissa</b>: una particolare
-                        arte marziale si applica a tutte le manovre di attacco o difesa, non una in particolare.
+                        specialit&agrave; dell'Abilit&agrave; <b>Accademiche</b> potrebbe essere <b>Storia</b>, una 
+                        di <b>Finanza</b> invece potrebbe essere <b>Criptomonete</b>, o <b>Mercato Azionario</b>. Non potr&agrave; 
+                        invece essere considerata una Specialit&agrave; consentita una forma di arte marziale per <b>Rissa</b>: 
+                        una particolare arte marziale si applica a tutte le manovre di attacco o difesa, non una in particolare.
                     </Typography>
 
                     <Typography paragraph style={guideStyle}>
-                        Ogni specialit&agrave; garantir&agrave; un dado supplementare 
-                        all'ammontare dei dadi applicato ai tiri per la specialit&agrave; del personaggio, e si potr&agrave; usare una sola 
-                        specialit&agrave; per tiro.
+                        Ogni specialit&agrave; garantir&agrave; un dado supplementare all'ammontare dei dadi applicato ai tiri 
+                        per la specialit&agrave; del personaggio, e si potr&agrave; usare una sola specialit&agrave; per tiro.
+                        Ogni personaggio ha diritto ad un numero variabile di Specialit&agrave; in fase di creazione. Per maggiori
+                        informazioni, consultare la <Link to={GuideRoutes.creation}>guida di creazione</Link>. Si potranno
+                        acquistare altre Specialit&agrave; per il personaggio con i punti esperienza, come descritto nella
+                        relativa <Link to={GuideRoutes.experience}>guida per l'esperienza</Link>.
                     </Typography>
                 </Grid>
             </Grid>
