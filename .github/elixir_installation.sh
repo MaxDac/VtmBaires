70ce21c00f61f55fad35faa308623ef4be0c95b6
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-01-05 09:36:56 +0000

    Updating the GitHub deploy file

--- .github/elixir_installation.sh
+++ .github/elixir_installation.sh
@@ -2,5 +2,5 @@ wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo 
 sudo apt-get update -y
 sudo apt-get install esl-erlang -y
 sudo apt-get install elixir -y
-mix local.hex -y
+mix local.hex --force -y
 mix archive.install hex phx_new
