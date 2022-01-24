00c00d5d442a046ead9f3048ff3dfe1f08488f1d
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-01-24 12:52:53 +0000

    Removed dependency from process for the log

--- apps/vtm_web/assets/src/_base/utils.js
+++ apps/vtm_web/assets/src/_base/utils.js
@@ -12,17 +12,12 @@ import type {SessionCharacter} from "../services/base-types";
 
 export type LogType = "log" | "info" | "warning" | "error";
 
-export const isDevelopment = (): boolean =>
-    process?.env?.NODE_ENV === "development";
-
 export const log = (message: string, obj?: any, type?: LogType): void => {
-    if (isDevelopment()) {
-        switch (type) {
-            case "error": return console.error(message, obj);
-            case "info": return console.info(message, obj);
-            case "warning": return console.warn(message, obj);
-            default: return console.debug(message, obj);
-        }
+    switch (type) {
+        case "error": return console.error(message, obj);
+        case "info": return console.info(message, obj);
+        case "warning": return console.warn(message, obj);
+        default: return console.debug(message, obj);
     }
 }
 
